93cb3dc9fc328dd1dd5959bbc0434f04c138e332
Jesús Muñoz <jmunto3@outlook.com>
2018-10-04 23:56:03 +0200

    HolaMundoNetBeans
    Proyecto de prueba en NetBeans

---
+++ HolaMundoEnNetBeans/src/holamundoennetbeans/HolaMundoEnNetBeans.java
@@ -0,0 +1,17 @@
+
+package holamundoennetbeans;
+
+/**
+ *
+ * @author jmunt
+ */
+public class HolaMundoEnNetBeans {
+
+ 
+    public static void main(String[] args) {
+        
+        System.out.println("Hola Mundo");
+        
+    }
+    
+}
